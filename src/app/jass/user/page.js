8a85e983b5aeb1f939fd72397f60962ad3101885
Jaspreet <jaspreetmehra185@gmail.com>
2024-03-13 22:13:06 +0530

    DB local to remote

--- src/app/jass/user/page.js
+++ src/app/jass/user/page.js
@@ -1,36 +1,10 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// 'use client'
+import React from 'react';
 
 export default function Users() {
-    const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    async function fetchSessionData() {
-      try {
-        // Make an API call to fetch session data
-        const response = await axios.get('/api/jass/sessions');
-        // Assuming the API returns user data
-        setUserData(response.data.user);
-      } catch (error) {
-        console.error('Error fetching session data:', error);
-      }
-    }
-
-    fetchSessionData();
-  }, []);
   return (
     <div>
       <h2>Session Data</h2>
-      {userData ? (
-        <div>
-          <p>User ID: {userData.id}</p>
-          <p>Username: {userData.username}</p>
-          {/* Display other user data as needed */}
-        </div>
-      ) : (
-        <p>Loading session data...</p>
-      )}
     </div>
   )
 }
--- src/database/config/db.js
+++ src/database/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 export default async function connectDB(){
-    const connect = await mongoose.connect("mongodb://localhost:27017/semple");
+    const URL = "mongodb+srv://jaspreetmehra185:RTN5jFawUs5f0h4v@cluster0.0do45ze.mongodb.net/jass?retryWrites=true&w=majority&appName=Cluster0"
+    const connect = await mongoose.connect(URL);
+    // const connect = await mongoose.connect("mongodb://localhost:27017/semple");
     if (!connect) {
         console.log("Not Connected DB")
     }
@@ -9,3 +11,6 @@ export default async function connectDB(){
         console.log("Connected DB");
     }
 }
+
+// RTN5jFawUs5f0h4v - admin
+// jaspreetmehra185
