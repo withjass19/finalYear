3f525c25e630f5dbdb5510b3de0ad5a0abacb4d7
Jaspreet <jaspreetmehra185@gmail.com>
2024-03-14 12:19:13 +0530

    update DB config

--- src/database/config/db.js
+++ src/database/config/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export default async function connectDB(){
     // const URL = "mongodb+srv://jaspreetmehra185:RTN5jFawUs5f0h4v@cluster0.0do45ze.mongodb.net/jass?retryWrites=true&w=majority&appName=Cluster0"
     // const connect = await mongoose.connect(URL);
-    const connect = await mongoose.connect("mongodb://localhost:27017/semple");
+    const connect = await mongoose.connect("mongodb://localhost:27017/semple", {
+        bufferCommands: false, // Disable command buffering
+        bufferTimeoutMS: 10000, // Set buffer timeout to 10 seconds
+      });
     if (!connect) {
         console.log("Not Connected DB")
     }
