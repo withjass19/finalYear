fe5b3ac233ca505dfb06a90a1dd0f8338a4808ab
Jaspreet <jaspreetmehra185@gmail.com>
2024-03-18 20:26:32 +0530

    update database configrations

--- src/database/config/db.js
+++ src/database/config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 export default async function connectDB(){
-    const URL = "mongodb+srv://jaspreetmehra185:7IBEcz0loRKVDCzf@cluster0.tcd4j4t.mongodb.net/semple?retryWrites=true&w=majority&appName=Cluster0"
-    const connect = await mongoose.connect(URL, { useUnifiedTopology: true})
-    // const connect = await mongoose.connect("mongodb://localhost:27017/semple")
+    // const URL = "mongodb+srv://jaspreetmehra185:7IBEcz0loRKVDCzf@cluster0.tcd4j4t.mongodb.net/semple?retryWrites=true&w=majority&appName=Cluster0"
+    // const connect = await mongoose.connect(URL, { useUnifiedTopology: true})
+    const connect = await mongoose.connect("mongodb://localhost:27017/semple")
     if (!connect) {
         console.log("Not Connected DB")
     }
